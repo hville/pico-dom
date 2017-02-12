fb83623d1ef5296f13151072bfc4eae2c2cf4bbe
hville <hugov@runbox.com>
2017-02-12 18:51:10 +0800

    fix

--- src/util/typ.js
+++ src/util/typ.js
@@ -1,13 +1,9 @@
+var G = require('../root/root')
+
 module.exports = typ
 
 function typ(t) {
 	return t == null ? t //eslint-disable-line eqeqeq
-		: (t.nodeName && t.nodeType > 0 && t.cloneNode) ? typ.E
+		: (t.nodeName && t.nodeType > 0 && t.cloneNode) ? G.Node
 		: t.constructor || Object
 }
-typ.A = Array
-typ.S = String
-typ.N = Number
-typ.F = Function
-typ.E = function(){} // for case where Node|Element is not yet defined
-typ.O = Object
