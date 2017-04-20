23e2e389ca49fd0feda3ccef688da3025d7cb953
hville <hugov@runbox.com>
2017-04-20 21:02:24 +0800

    grouping

--- src/create-element.js
+++
@@ -1,9 +0,0 @@
-import {creator} from './util/creator'
-import {decorate} from './decorate'
-import {namespaces} from './namespaces'
-
-var presetElement = creator(decorate)
-
-export var createElement = presetElement()
-createElement.svg = presetElement({xmlns: namespaces.svg})
-createElement.preset = presetElement
--- src/create-node.js
+++ src/create-node.js
@@ -1,4 +1,13 @@
 import {defaultView} from './default-view'
+import {creator} from './util/creator'
+import {decorate} from './decorate'
+import {namespaces} from './namespaces'
+
+var presetElement = creator(decorate)
+
+export var createElement = presetElement()
+createElement.svg = presetElement({xmlns: namespaces.svg})
+createElement.preset = presetElement
 
 /**
 * @function comment
